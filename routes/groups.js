6aad8113ba8ae0bc99096b1bd3140e90adc8237d
ashirs-ms-office <ashirs@microsoft.com>
2015-07-14 18:59:46 -0700

    Initial checkin

---
+++ routes/groups.js
@@ -0,0 +1,14 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET groups */
+router.get('/', function (req, res) {
+    var responseJson = { groups: "Listing all the groups" };
+    res.json(responseJson)
+});
+
+router.post(function (req, res) {
+    var x = req.body;
+});
+
+module.exports = router;
